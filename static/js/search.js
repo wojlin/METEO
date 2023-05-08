70efb81785a208951fba20a97afebce5f6107deb
wojlin <wojciech.linowski.2137@gmail.com>
2023-05-08 18:29:33 +0200

    working on chart generation

--- static/js/search.js
+++ static/js/search.js
@@ -25,6 +25,215 @@ class Meteo
         xmlHttp.send(null);
     }
 
+    hide_transition()
+    {
+        let element = document.getElementById("transition");
+        element.classList.remove("transition_animate");
+        element.classList.add("transition_hidden");
+
+        let element1 = document.getElementById("clouds");
+        element1.classList.remove("clouds_animate");
+        element1.classList.add("clouds_hidden");
+    }
+
+    includes(value)
+    {
+        if(this.parameters.includes(value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    display_meteo(response)
+    {
+        let json = JSON.parse(response);
+        console.log(json);
+
+        const ctx = document.getElementById('temperature_chart');
+
+        let amount = Object.keys(json["hourly"]["time"]).length;
+
+        let cut_start = 0;
+        let cut_end = 0;
+
+        if(amount <= 24)
+        {
+            cut_start = 11;
+            cut_end = 0;
+        }
+        else if(amount <= 24*4)
+        {
+            cut_start = 8;
+            cut_end = 3;
+        }
+        else
+        {
+            cut_start = 5;
+            cut_end = 6;
+        }
+
+        for (const [key, value] of Object.entries(json["hourly"]["time"])) {
+          json["hourly"]["time"][key] = value.toString().substring(cut_start, value.toString().length-cut_end);
+          json["hourly"]["time"][key] = json["hourly"]["time"][key].replace("T", " H");
+        }
+
+
+
+
+        const chartAreaBorder = {
+          id: 'chartAreaBorder',
+          beforeDraw(chart, args, options) {
+            const {ctx, chartArea: {left, top, width, height}} = chart;
+            ctx.save();
+            ctx.strokeStyle = options.borderColor;
+            ctx.lineWidth = options.borderWidth;
+            ctx.setLineDash(options.borderDash || []);
+            ctx.lineDashOffset = options.borderDashOffset;
+            ctx.strokeRect(left, top, width, height);
+            ctx.restore();
+          }
+        };
+
+
+        if(meteo.includes("temperature_2m") && meteo.includes("apparent_temperature"))
+        {
+            new Chart(ctx,
+            {
+                type: 'line',
+                data:
+                {
+                    labels: json["hourly"]["time"],
+                    datasets:
+                    [
+                        {
+                            label: 'temperature',
+                            data: json["hourly"]["temperature_2m"],
+                            borderWidth: 3,
+                            borderColor: '#f78d9b',
+                            backgroundColor: '#f78d9b',
+                        },
+                        {
+                            label: 'apparent temperature',
+                            data: json["hourly"]["apparent_temperature"],
+                            borderWidth: 2,
+                            borderColor: '#8da8f7',
+                            backgroundColor: '#8da8f7',
+                        }
+                    ]
+                },
+                options:
+                {
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    tooltips:
+                    {
+                        enabled: false
+                    },
+                    hover:
+                    {
+                        mode: null
+                    },
+                    events:
+                    [
+
+                    ],
+                    plugins:
+                    {
+                        legend:
+                        {
+                            labels:
+                            {
+                                color: '#e0aaff'
+                            }
+                        },
+                        chartAreaBorder:
+                        {
+                            borderColor: '#e0aaff',
+                            borderWidth: 2,
+                        }
+                    },
+                    scales:
+                    {
+                        x:
+                        {
+                            beginAtZero: true,
+                            ticks:
+                            {
+                                color: '#e0aaff',
+                                beginAtZero: true
+                            },
+                            grid:
+                            {
+                                display: true,
+                                drawOnChartArea: true,
+                                drawTicks: true,
+                                color: "rgba(224, 170, 255, 0.5)",
+                            },
+                            border:
+                            {
+                                dash: [2,4],
+                            }
+                        },
+                        y:
+                        {
+                            beginAtZero: true,
+                            ticks:
+                            {
+                                color: '#e0aaff',
+                                beginAtZero: true,
+                                callback: function(value, index, ticks)
+                                {
+                                    return value+"°C";
+                                }
+                            },
+                            grid:
+                            {
+                                display: true,
+                                drawOnChartArea: true,
+                                drawTicks: true,
+                                color: "rgba(224, 170, 255, 0.5)",
+
+                            },
+                            border:
+                            {
+                                dash: [2,4],
+                            }
+                        }
+                    },
+                    elements:
+                    {
+                        point:
+                        {
+                            radius: 0
+                        }
+                    }
+                },
+                plugins: [chartAreaBorder]
+            }
+            );
+        }
+
+
+
+
+
+        document.getElementById("settings_container").style.display = "none";
+        document.getElementById("meteo_container").style.display = "block";
+        let element = document.getElementById("transition");
+        element.classList.remove("transition_animate");
+        element.classList.add("transition_hidden");
+
+        let element1 = document.getElementById("clouds");
+        element1.classList.remove("clouds_animate");
+        element1.classList.add("clouds_hidden");
+
+
+    }
+
     prognose()
     {
         this.check_forecast_selection();
@@ -62,19 +271,12 @@ class Meteo
             let url = url_base + url_pos + url_params + url_time + url_timezone
 
             console.log(url)
+            setTimeout(this.get, 5000, url, this.display_meteo);
+
         }
     }
 
-    hide_transition()
-    {
-        let element = document.getElementById("transition");
-        element.classList.remove("transition_animate");
-        element.classList.add("transition_hidden");
 
-        let element1 = document.getElementById("clouds");
-        element1.classList.remove("clouds_animate");
-        element1.classList.add("clouds_hidden");
-    }
 
     check_forecast_selection()
     {
